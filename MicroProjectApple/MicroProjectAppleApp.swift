2285c543260f6eb7b3faf572e33042741cba485b
shoug <sho0oshkh@gmail.com>
2024-04-23 11:01:45 +0300

    Initial Commit

---
+++ MicroProjectApple/MicroProjectAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroProjectAppleApp.swift
+//  MicroProjectApple
+//
+//  Created by shoug on 14/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MicroProjectAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
